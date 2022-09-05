49942368527248152fc33733a5409a120b2affe1
Алексей <stormwww@yandex.ru>
2022-09-05 17:31:04 +0300

    popular posts

---
+++ website/blog/templatetags/sidebar.py
@@ -0,0 +1,9 @@
+from django import template
+from blog.models import Post
+
+register = template.Library()
+
+@register.inclusion_tag('blog/popular_posts_tpl.html')
+def get_popular(cnt=3):
+    posts = Post.objects.order_by('created_at')[:cnt]
+    return {'posts': posts}
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Group, Author, Comment
-from random import randint
+from random import randint, choice
 from .forms import PostForm, CommentForm
 from django.views.generic.edit import CreateView
 from django.http import HttpResponse, HttpResponseRedirect
@@ -59,13 +59,9 @@ def show_post(request, post_slug):
                     replay_comment.reply_to = parent_obj.author
 
             comment = form.save(commit=False)
-
-
             comment.post = post
             comment.author = request.user
             comment.photo = author.photo
-
-
             comment.save()
             return redirect(post.get_absolute_url())
     else:
@@ -78,8 +74,6 @@ def show_post(request, post_slug):
         'comments': comments,
         'form': form,
     })
-
-
 
 
 def show_groups(request, post_slug):
@@ -183,32 +177,17 @@ def post_edit(request, post_slug):
     return render(request, 'blog/create_post.html', context)
 
 
-# def add_comment(request, post_slug, parent_comment_id=None):
-#     if request.method == "POST":
-#         post = get_object_or_404(Post, slug=post_slug, user=request.user)
-#         form = CommentForm(request.POST)
-#         if form.is_valid():
-#             comment = form.save(commit=False)
-#             comment.post = post
-#             comment.user = request.user
-#             if parent_comment_id:
-#                 parent_comment = Comment.objects.get(id=parent_comment_id)
-#                 comment.parent_id = parent_comment.get_root().id
-#                 comment.reply_to = parent_comment.user
-#                 comment.save()
-#
-#                 return HttpResponse('200 OK')
-#
-#             comment.save()
-#             return HttpResponseRedirect(post.get_absolute_url())
-#     else:
-#         form = CommentForm()
-#         context = {
-#             'form': form,
-#             'post_slug': post_slug,
-#             'parent_comment_id': parent_comment_id,
-#         }
-#     return render(request, 'blog/single.html', context)
+def sidebar(request):
+    template = 'includes/sidebar.html'
+    authors = Author.objects.all()
+    rand_author = choice(authors)
+
+
+    context = {
+        'authors': authors,
+        'rand_author': rand_author
+    }
+    return render(request, template, context)
 
 
 
