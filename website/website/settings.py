db630aa5e3cf24ec12b14364efe0e766575ecb3e
Алексей <stormwww@yandex.ru>
2022-11-03 14:39:23 +0300

    pagination

--- website/website/settings.py
+++ website/website/settings.py
@@ -181,6 +181,9 @@ INTERNAL_IPS = [
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
